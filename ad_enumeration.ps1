5a6f76127a04490a8f8a7d7a01275558fee37f18
l4rry <l4rryxz@gmail.com>
2023-07-28 14:47:11 +0200

    enumeration script edits

--- ad_enumeration.ps1
+++ ad_enumeration.ps1
@@ -1,4 +1,30 @@
-$PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
-$DN = ([adsi]'').distinguishedName 
-$LDAP = "LDAP://$PDC/$DN"
-$LDAP
+function LDAPSearch {
+    param (
+        [string]$LDAPQuery
+    )
+
+    $PDC = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
+    $DistinguishedName = ([adsi]'').distinguishedName
+
+    $DirectoryEntry = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$PDC/$DistinguishedName")
+
+    $DirectorySearcher = New-Object System.DirectoryServices.DirectorySearcher($DirectoryEntry, $LDAPQuery)
+
+    return $DirectorySearcher.FindAll()
+
+}
+
+####
+#### Usage:
+####
+
+# PS> Import-Module .\ad_enumeration.ps1
+#
+# PS> LDAPSearch -LDAPQuery "(samAccountType=805306368)"
+#
+# PS> LDAPSearch -LDAPQuery "(objectclass=group)"
+#
+#
+# > foreach ($group in $(LDAPSearch -LDAPQuery "(objectCategory=group)")) {
+# >> $group.properties | select {$_.cn}, {$_.member}
+# >> }
